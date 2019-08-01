683f91df0e471eb8113438d06bc30a53f2f312aa
howiezhao <howiezhaohr@hotmail.com>
2019-08-01 21:20:49 +0800

    add powershell and ps1 script

---
+++ link.ps1
@@ -0,0 +1,5 @@
+# Link for Sublime
+New-Item -Path "C:\Users\howie\AppData\Roaming\Sublime Text 3\Packages\User\Preferences.sublime-settings" -ItemType SymbolicLink -Value "C:\dotfiles\sublimetext3\Preferences.sublime-settings"
+
+# Link for PowerShell
+New-Item -Path "C:\Users\howie\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -ItemType SymbolicLink -Value "C:\dotfiles\powershell\Microsoft.PowerShell_profile.ps1"
