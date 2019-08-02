3686f52ab9b60958a258ea315b2ec2db76939112
howiezhao <howiezhaohr@hotmail.com>
2019-08-02 23:25:20 +0800

    update link.ps1

--- link.ps1
+++ link.ps1
@@ -1,5 +1,9 @@
 # Link for Sublime
-New-Item -Path "C:\Users\howie\AppData\Roaming\Sublime Text 3\Packages\User\Preferences.sublime-settings" -ItemType SymbolicLink -Value "C:\dotfiles\sublimetext3\Preferences.sublime-settings"
+$sourcesublime = ($pwd).Path + "\sublimetext3\Preferences.sublime-settings"
+$targetsublime = $env:APPDATA + "\Sublime Text 3\Packages\User\Preferences.sublime-settings"
+New-Item -Path $targetsublime -ItemType SymbolicLink -Value $sourcesublime
 
 # Link for PowerShell
-New-Item -Path "C:\Users\howie\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" -ItemType SymbolicLink -Value "C:\dotfiles\powershell\Microsoft.PowerShell_profile.ps1"
+$sourceps = ($pwd).Path + "\powershell\Microsoft.PowerShell_profile.ps1"
+$targetps = $env:HOMEPATH + "\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+New-Item -Path $targetps -ItemType SymbolicLink -Value $sourceps
