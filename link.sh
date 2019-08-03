96d435747fdce5c933372756bee8eb0f9156a8cd
Howie Zhao <howiezhaohr@hotmail.com>
2019-08-03 09:03:19 +0800

    update link.sh

---
+++ link.sh
@@ -0,0 +1,11 @@
+# Link for tmux
+ln -sf -b tmux/tmux.conf ~/.tmux.conf
+
+# Link for bash
+ln -sf -b bash/bashrc ~/.bashrc
+
+# Link for zsh
+ln -sf -b zsh/zshrc ~/.zshrc
+
+# Link for gdb
+ln -sf -b gdb/gdbinit ~/.gdbinit
