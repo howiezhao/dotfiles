93883d1a4f653b69e5c8931cef0406acf21b7e74
Howie Zhao <howiezhaohr@hotmail.com>
2019-10-15 18:39:44 +0800

    fix bug

--- link.sh
+++ link.sh
@@ -1,11 +1,14 @@
 # Link for tmux
-ln -sf -b tmux/tmux.conf ~/.tmux.conf
+ln -sf -b ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # Link for bash
-ln -sf -b bash/bashrc ~/.bashrc
+ln -sf -b ~/dotfiles/bash/bashrc ~/.bashrc
 
 # Link for zsh
-ln -sf -b zsh/zshrc ~/.zshrc
+ln -sf -b ~/dotfiles/zsh/zshrc ~/.zshrc
 
 # Link for gdb
-ln -sf -b gdb/gdbinit ~/.gdbinit
+ln -sf -b ~/dotfiles/gdb/gdbinit ~/.gdbinit
+
+# Link for Sublime Text 3
+ln -sf -b ~/dotfile/sublimetext3/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
