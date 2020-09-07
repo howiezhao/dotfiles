a8b1df4cd901f0be7c5177f33066957a52e20876
howiezhao <howiezhaohr@hotmail.com>
2020-09-07 12:55:10 +0800

    update link

--- link.sh
+++ link.sh
@@ -1,16 +1,19 @@
 # Link for tmux
-ln -sf -b ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -sf -b ./tmux/tmux.conf ~/.tmux.conf
 
 # Link for bash
-ln -sf -b ~/dotfiles/bash/bashrc ~/.bashrc
+ln -sf -b ./bash/bashrc ~/.bashrc
 
 # Link for zsh
-ln -sf -b ~/dotfiles/zsh/zshrc ~/.zshrc
+ln -sf -b ./zsh/zshrc ~/.zshrc
 
 # Link for gdb
-ln -sf -b ~/dotfiles/gdb/gdbinit ~/.gdbinit
+ln -sf -b ./gdb/gdbinit ~/.gdbinit
+
+# Link for vim
+ln -sf -b ./vim/vimrc ~/.vimrc
 
 # Link for Sublime Text 3
-ln -sf -b ~/dotfiles/sublimetext3/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
+ln -sf -b ./sublimetext3/Preferences.sublime-settings ~/.config/sublime-text-3/Packages/User/Preferences.sublime-settings
 
-ln -sf -b ~/dotfiles/sublimetext3/'Package Control.sublime-settings' ~/.config/sublime-text-3/Packages/User/'Package Control.sublime-settings'
+ln -sf -b ./sublimetext3/'Package Control.sublime-settings' ~/.config/sublime-text-3/Packages/User/'Package Control.sublime-settings'
